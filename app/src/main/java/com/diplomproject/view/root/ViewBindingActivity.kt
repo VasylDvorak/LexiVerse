5a32b76ed1fbc90f173e924719e6430c9e82547e
ocusnir <102429445+ocusnir@users.noreply.github.com>
2023-08-07 13:43:38 +0300

    SplashViewModel.kt

--- app/src/main/java/com/diplomproject/view/root/ViewBindingActivity.kt
+++ app/src/main/java/com/diplomproject/view/root/ViewBindingActivity.kt
@@ -2,24 +2,28 @@ package com.diplomproject.view.root
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.viewbinding.ViewBinding
+import com.diplomproject.model.splash_fetching_data.SplashViewModel
 
 open class ViewBindingActivity<T : ViewBinding>(
-    private val inflateBinding: (inflater: LayoutInflater) -> T
-) : AppCompatActivity() {
+    private val inflateBinding: (inflater: LayoutInflater) -> T) : AppCompatActivity() {
     private var _binding: T? = null
 
+    private val viewModel: SplashViewModel by viewModels()
     protected val binding: T
         get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //SplashScreen
-        Thread.sleep(3000)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
 
         _binding = inflateBinding.invoke(layoutInflater)
 
