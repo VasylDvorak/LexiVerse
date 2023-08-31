c279b0cd7c0b8946075f966a1f28f14e4c7034a4
Vasyl <dvorak1981@yandex.ru>
2023-08-31 18:23:43 +0300

    Убрал отправка по SMS

--- app/src/main/java/com/diplomproject/view/settings_menu/ShareApplicationFragment.kt
+++ app/src/main/java/com/diplomproject/view/settings_menu/ShareApplicationFragment.kt
@@ -1,18 +1,12 @@
 package com.diplomproject.view.settings_menu
 
-import android.Manifest
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.telephony.SmsManager
 import android.view.View
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.diplomproject.R
 import com.diplomproject.databinding.FragmentShareBinding
 
@@ -28,9 +22,6 @@ class ShareApplicationFragment : BaseFragmentSettingsMenu<FragmentShareBinding>(
 
     private fun initClickedViews() {
         binding.apply {
-
-            imageViewViaSMS.setOnClickListener { sendSMS() }
-            sendSMS.setOnClickListener { sendSMS() }
             copyButton.setOnClickListener {
                 val clipboardManager =
                     requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
@@ -51,45 +42,6 @@ class ShareApplicationFragment : BaseFragmentSettingsMenu<FragmentShareBinding>(
 
             sendButton.setOnClickListener {
                 shareApp()
-            }
-        }
-    }
-
-    private fun sendSMS() {
-        binding.apply {
-            val phoneNumber = enterPhoneNumber.text.toString()
-            val message = getString(R.string.for_sms) + webLink.text.toString()
-            try {
-                val smsManager: SmsManager
-                if (Build.VERSION.SDK_INT >= 23) {
-                    smsManager = requireActivity().getSystemService(SmsManager::class.java)
-                } else {
-                    smsManager = SmsManager.getDefault()
-                }
-                if (ContextCompat.checkSelfPermission(
-                        requireActivity(),
-                        Manifest.permission.SEND_SMS
-                    )
-                    != PackageManager.PERMISSION_GRANTED
-                ) {
-                    ActivityCompat.requestPermissions(
-                        requireActivity(),
-                        arrayOf(Manifest.permission.SEND_SMS),
-                        1
-                    );
-                } else {
-                    smsManager.sendTextMessage(
-                        phoneNumber, null, message,
-                        null, null
-                    )
-                    Toast.makeText(
-                        context, getString(R.string.message_were_sent),
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            } catch (e: Exception) {
-                Toast.makeText(context, getString(R.string.enter_all_data), Toast.LENGTH_LONG)
-                    .show()
             }
         }
     }
